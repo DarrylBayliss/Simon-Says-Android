fea6d72706bc1e4fa5b0fbc0cfef7a2a9ba44534
Darryl Bayliss <darryl@darrylbayliss.net>
2024-05-01 19:51:10 +0100

    Update prompts.

--- app/src/main/java/com/darrylbayliss/simonsays/data/MediapipeLLMDataSource.kt
+++ app/src/main/java/com/darrylbayliss/simonsays/data/MediapipeLLMDataSource.kt
@@ -11,21 +11,19 @@ import javax.inject.Inject
 
 
 private const val SimonSaysPrompt = """
-    You are a Simon in a game of Simon Says. Your objective is to ask the player to perform tasks.
+    You are a Simon in a game of Simon Says.
     
     For every task you give, you must prefix it with the words "Simon says".
     
     You must not ask the player to do anything that is dangerous, unethical or unlawful.
-    
-    Do not try to communicate with the player. Only ask the player to perform tasks.
 """
 
 private const val MovePrompt = SimonSaysPrompt + """
-    Give the player a task related to moving to a different position.
+    Ask the player to move to a different position.
 """
 
 private const val TouchBodyPartPrompt = SimonSaysPrompt + """
-    Give the player a task related to touching a body part.
+    Ask the player to touch an unoffensive body part.
 """
 
 private const val SingASongPrompt = SimonSaysPrompt + """
@@ -33,11 +31,11 @@ private const val SingASongPrompt = SimonSaysPrompt + """
 """
 
 private const val TakePhotoPrompt = SimonSaysPrompt + """
-    Give the player a task to take a photo of an object.
+   Ask the player to take a photo of an object.
 """
 
 private const val MakeANoisePrompt = SimonSaysPrompt + """
-    Give the player a task to make a loud noise.
+    Ask the player to make a loud noise.
 """
 
 class MediapipeLLMDataSource @Inject constructor(
