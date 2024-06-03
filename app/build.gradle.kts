6ba62c50033fe31891b6d7f299532475f5abf072
Darryl Bayliss <darryl@darrylbayliss.net>
2024-06-03 12:24:28 +0100

    Remove unneeded navigation safe args plugin.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     kotlin("kapt")
     kotlin("plugin.serialization") version "2.0.0"
     id("com.google.dagger.hilt.android")
-    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
